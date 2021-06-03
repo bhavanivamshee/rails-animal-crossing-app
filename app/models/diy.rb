d8fdfcee6f1dcd9882adb12a4e650f8196e87ddb
Bhavani <bhavanivamshee@gmail.com>
2021-06-03 15:58:26 -0500

    adds seed data for diy and materials

--- app/models/diy.rb
+++ app/models/diy.rb
@@ -1,4 +1,28 @@
 class Diy < ApplicationRecord
     has_many :materials
     accepts_nested_attributes_for :materials
+
+    def self.get_data
+        resp = RestClient::Request.execute(method: :get,
+        url: "https://api.nookipedia.com/nh/recipes",
+        headers:{
+            'X-API-KEY': ENV['NOOK_KEY']
+                })
+        diy_data = JSON.parse(resp.body)
+        diy_data.each do |diy|
+            @params = { diy: 
+            {
+                name: diy["name"],
+                url: diy["url"],
+                image_url: diy["image_url"],
+                recipes_to_unlock: diy["recipes_to_unlock"],
+                materials_diy: diy["materials"],
+                materials_attributes: diy["materials"]
+                }
+            }
+
+            Diy.create!(@params[:diy])
+
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Material.destroy_all
+Diy.destroy_all
+
+Diy.get_data
