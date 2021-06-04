a75b66312fec916f480d1723c99d46f40101545e
Bhavani <bhavanivamshee@gmail.com>
2021-06-04 00:10:05 -0500

    adds materials new form

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -3,4 +3,11 @@ class MaterialsController < ApplicationController
     def index
         @materials = Material.all
     end
+
+    def new
+        @material = Material.new
+    end
+
+    def create
+    end
 end
