fac1134d2a43d3d9e408fa151f7dac02b6c03f31
Bhavani <bhavanivamshee@gmail.com>
2021-06-04 02:37:33 -0500

    adds sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  resources :users do 
+  resources :users, except: [:new] do 
     resources :diys
   end
   resources :materials
   resources :diys do 
     resources :materials
   end
+
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
