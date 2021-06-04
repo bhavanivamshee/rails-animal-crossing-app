a051af1957802ed21af50be5e40ddc6dca7c8784
Bhavani <bhavanivamshee@gmail.com>
2021-06-04 01:24:37 -0500

    updates diy show and controller for user

--- app/controllers/diys_controller.rb
+++ app/controllers/diys_controller.rb
@@ -1,11 +1,16 @@
 class DiysController < ApplicationController
 
     def index
+        if params[:user_id]
+        user = User.find_by(id: params[:user_id])
+        @diys = user.diys
+        else
         @diys = Diy.all
+        end
     end
 
     def show
         @diy = Diy.find_by_id(params[:id])
     end
-    
+
 end
