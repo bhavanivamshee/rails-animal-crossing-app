5137f0b1efb1cb9efd1fb2d9d47a73f5080e369e
Bhavani <bhavanivamshee@gmail.com>
2021-06-04 02:55:17 -0500

    adds borders for error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to diys_path 
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:island_name, :username, :email, :dream_address, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :materials
     has_many :diys, through: :materials
 
+    validates_presence_of :username
     validates_uniqueness_of :username
 end
